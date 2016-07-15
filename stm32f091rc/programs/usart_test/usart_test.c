273ef32230ee23edacac2a88bf88396b559489e8
Engi-xm <kristijonas.a@gmail.com>
2016-07-15 18:54:21 +0300

    basic usart rx/tx working

--- stm32f091rc/programs/usart_test/usart_test.c
+++ stm32f091rc/programs/usart_test/usart_test.c
@@ -4,8 +4,9 @@
 #define USART_DELIMITER '\0'
 
 void init_leds(void);
-void init_usart4(uint16_t baud);
-void usart4_send_byte(uint8_t data);
+void init_usart2(uint16_t baud);
+void usart2_send_byte(uint8_t data);
+uint8_t usart2_read_byte(void);
 // void usart2_stop_tx(void);
 // uint8_t usart2_send(uint8_t* data_ptr);
 // uint8_t peek(uint8_t* ptr);
@@ -33,15 +34,19 @@ int main(void) {
 	// NVIC_EnableIRQ(USART2_IRQn);
 	// NVIC_SetPriority(USART2_IRQn, 2);
 	init_leds();
-	init_usart4(9600);
+	init_usart2(9600);
 	init_delay();
 	// uint8_t send_data[3] = {0x61, 0x62, '\0'};
 	uint8_t data_byte = 0x63;
 
 	// loop
 	while(1) {
-		_delay_ms(200);
-		usart4_send_byte(data_byte);
+		if((USART2->ISR & USART_ISR_RXNE) == USART_ISR_RXNE) {
+			GPIOB->ODR = usart2_read_byte();
+		}
+		_delay_us(500);
+		// _delay_ms(200);
+		// usart2_send_byte(data_byte);
 	}
 
 	return 0;
@@ -50,25 +55,29 @@ int main(void) {
 void init_leds(void) {
 	RCC->AHBENR |= RCC_AHBENR_GPIOBEN; // turn clock on
 	GPIOB->MODER |= 0x5555; // set 8 pins to output
-	GPIOB->OTYPER &= ~(0x00); // set to push-pull
-	GPIOB->OSPEEDR &= ~(0x0000); // set low speed
+	GPIOB->OTYPER &= ~(0x11); // set to push-pull
+	GPIOB->OSPEEDR &= ~(0x1111); // set low speed
 }
 
-void init_usart4(uint16_t baud) {
+void init_usart2(uint16_t baud) {
 	// init pins
     RCC->AHBENR |= RCC_AHBENR_GPIOAEN; // start clock
-    GPIOA->MODER |= (GPIO_MODER_MODER1_1 | GPIO_MODER_MODER0_1); // set af mode
-    GPIOA->AFR[0] |= 0x44U; // set af4
-    GPIOA->PUPDR |= 0x5U; // set pull ups
+    GPIOA->MODER |= (GPIO_MODER_MODER3_1 | GPIO_MODER_MODER2_1); // set af mode
+    GPIOA->AFR[0] |= 0x1100U; // set af1
+    GPIOA->PUPDR |= 0x50U; // set pull ups
 
     // init usart
-    RCC->APB1ENR |= RCC_APB1ENR_USART4EN; // start clock
-    USART4->BRR = SystemCoreClock / baud; // set baud rate
-    USART4->CR1 |= (USART_CR1_TE | USART_CR1_UE); // enable periph
+    RCC->APB1ENR |= RCC_APB1ENR_USART2EN; // start clock
+    USART2->BRR = SystemCoreClock / baud; // set baud rate
+    USART2->CR1 |= (USART_CR1_TE | USART_CR1_RE | USART_CR1_UE); // enable periph
 }
 
-void usart4_send_byte(uint8_t data) {
-	USART4->TDR = data; // load data to buffer
+void usart2_send_byte(uint8_t data) {
+	USART2->TDR = data; // load data to buffer
+}
+
+uint8_t usart2_read_byte(void) {
+	return (uint8_t)USART2->RDR;
 }
 
 // void usart2_stop_tx(void) {
