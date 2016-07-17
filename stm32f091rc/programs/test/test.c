617aa5cd9ab745a26aa1677d20f449df41e4cbfa
Engi-xm <kristijonas.a@gmail.com>
2016-07-17 18:31:10 +0300

    adc w/ dma working

--- stm32f091rc/programs/test/test.c
+++ stm32f091rc/programs/test/test.c
@@ -12,7 +12,7 @@ void init_tim6(void); // interrupt timer
 void init_leds(void);
 void init_pwm(void);
 void init_adc(void);
-uint16_t read_adc(void);
+void read_adc(uint16_t* adc_values);
 void set_pwm(uint8_t i);
 void itoa(uint8_t* str, uint8_t len, uint32_t val);
 
@@ -30,16 +30,20 @@ int main() {
 	// init_pwm();
 	// init_tim3();
  	
- 	uint16_t adc_value;
+ 	uint16_t adc_values[3];
  	uint8_t adc_value_str[5];
  	uint8_t new_line[3] = {'\n', '\r','\0'};
+ 	uint8_t space[2] = {0x20,'\0'};
 
  	while(1) {
- 		adc_value = read_adc();
- 		itoa(adc_value_str, 4, adc_value);
+ 		read_adc(adc_values);
+ 		itoa(adc_value_str, 4, adc_values[0]);
+ 		usart2_send(adc_value_str);
+ 		usart2_send(space);
+ 		itoa(adc_value_str, 4, adc_values[1]);
  		usart2_send(adc_value_str);
  		usart2_send(new_line);
- 		GPIOB->ODR = (adc_value >> 4);
+ 		GPIOB->ODR = (adc_values[0] >> 4);
  		_delay_ms(200);
  	}
 
@@ -58,6 +62,20 @@ void init_adc(void) {
 	// init pin
 	RCC->AHBENR |= RCC_AHBENR_GPIOAEN; // turn clock on
 	GPIOA->MODER |= (GPIO_MODER_MODER5_1 | GPIO_MODER_MODER5_0); // set analog mode
+	GPIOA->MODER |= (GPIO_MODER_MODER0_1 | GPIO_MODER_MODER0_0); // set analog mode
+
+	// init dma
+	RCC->AHBENR |= RCC_AHBENR_DMA2EN; // turn clock on
+	DMA2->CSELR |= (DMA2_CSELR_CH5_ADC); // set channel selection
+	// init channel5
+	DMA2_Channel5->CCR |= DMA_CCR_PL_0; // set medium priority
+	DMA2_Channel5->CCR |= DMA_CCR_MINC; // set memory increment mode
+	DMA2_Channel5->CCR |= DMA_CCR_CIRC; // turn on circular mode
+	DMA2_Channel5->CCR |= DMA_CCR_MSIZE_0; // set 16bit memory size
+	DMA2_Channel5->CCR |= DMA_CCR_PSIZE_0; // set 16bit periph size
+	DMA2_Channel5->CCR &= ~(DMA_CCR_DIR); // set read fron periph
+	DMA2_Channel5->CPAR = (uint32_t)&(ADC1->DR); // set periph address
+	DMA2_Channel5->CNDTR = 0x2UL; // set to two channels
 
 	// init adc
 	RCC->APB2ENR |= RCC_APB2ENR_ADCEN; // turn clock on
@@ -70,32 +88,33 @@ void init_adc(void) {
 	while((ADC1->CR & ADC_CR_ADCAL) == ADC_CR_ADCAL); // wait for calibration
 	ADC1->CR |= ADC_CR_ADEN; // enable
 	while((ADC1->ISR & ADC_ISR_ADRDY) == ADC_ISR_ADRDY); // wait for enable
-	ADC1->CHSELR |= ADC_CHSELR_CHSEL5; // select ch5
+	ADC1->CHSELR |= (ADC_CHSELR_CHSEL5 | ADC_CHSELR_CHSEL0); // select ch5 and ch0
 	ADC1->CFGR1 |= ADC_CFGR1_AUTOFF; // enable auto off
+	ADC1->CFGR1 |= (ADC_CFGR1_DMAEN | ADC_CFGR1_DMACFG); // enable circular dma
 	ADC1->SMPR |= ADC_SMPR_SMP_2; // set sampling time
 }
 
 void init_pwm(void) {
 	// init timer
-    RCC->APB1ENR |= RCC_APB1ENR_TIM3EN; // start clock
-    TIM3->CR1 |= TIM_CR1_ARPE; // enable preload
-    TIM3->PSC = 480 - 1; // 100kHz
-    TIM3->ARR = 100 - 1; // number of steps
-    TIM3->CCR1 = 80; // duty cycle
+	RCC->APB1ENR |= RCC_APB1ENR_TIM3EN; // start clock
+	TIM3->CR1 |= TIM_CR1_ARPE; // enable preload
+	TIM3->PSC = 480 - 1; // 100kHz
+	TIM3->ARR = 100 - 1; // number of steps
+	TIM3->CCR1 = 80; // duty cycle
 
-    // init output
-    TIM3->CCMR1 |= (TIM_CCMR1_OC1M_2 | TIM_CCMR1_OC1M_1); // set pwm mode 1
-    TIM3->CCMR1 |= TIM_CCMR1_OC1PE; // enable preload
-    TIM3->CCER |= TIM_CCER_CC1E; // enable output
+	// init output
+	TIM3->CCMR1 |= (TIM_CCMR1_OC1M_2 | TIM_CCMR1_OC1M_1); // set pwm mode 1
+	TIM3->CCMR1 |= TIM_CCMR1_OC1PE; // enable preload
+	TIM3->CCER |= TIM_CCER_CC1E; // enable output
 
-    // init pin
-    RCC->AHBENR |= RCC_AHBENR_GPIOBEN; // start clock
-    GPIOB->MODER |= GPIO_MODER_MODER4_1; // set af mode
-    GPIOB->AFR[0] |= 0x10000U; // set af1
+	// init pin
+	RCC->AHBENR |= RCC_AHBENR_GPIOBEN; // start clock
+	GPIOB->MODER |= GPIO_MODER_MODER4_1; // set af mode
+	GPIOB->AFR[0] |= 0x10000U; // set af1
 
-    // final init
-    TIM3->EGR |= TIM_EGR_UG; // generate update to load registers
-    TIM3->CR1 |= TIM_CR1_CEN; // start timer
+	// final init
+	TIM3->EGR |= TIM_EGR_UG; // generate update to load registers
+	TIM3->CR1 |= TIM_CR1_CEN; // start timer
 }
 
 void init_tim6(void) {
@@ -123,17 +142,17 @@ void set_pwm(uint8_t i) {
 	TIM3->CCR1 = i;
 }
 
-uint16_t read_adc(void) {
+void read_adc(uint16_t* adc_values) {
 	while((ADC1->CR & ADC_CR_ADSTART) == ADC_CR_ADSTART); // check if available
+	DMA2_Channel5->CMAR = (uint32_t)adc_values; // set memory address
+	DMA2_Channel5->CCR |= DMA_CCR_EN; // enable periph
 	ADC1->CR |= ADC_CR_ADSTART; // start conversion
-	while((ADC1->ISR & ADC_ISR_EOS) == 0); // wait for conversion
-	return (uint16_t)ADC1->DR; // return result
 }
 
 void itoa(uint8_t* str, uint8_t len, uint32_t val) {
 	for(uint8_t i = 1; i <= len; i++) {
-		str[len - i] = (uint8_t)((val % 10UL) + '0');
-		val /= 10;
+		str[len - i] = (uint8_t)((val % 10UL) + '0'); // set char
+		val /= 10; // cycle
 	}
-	str[len] = '\0';
+	str[len] = '\0'; // close string
 }
