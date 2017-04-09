e69fcaf2eeb31317ae2c5ec9a666c210894df1ab
Engi-xm <kristijonas.a@gmail.com>
2017-04-09 13:57:57 +0300

    working dma usart lib

--- stm32f091rc/programs/test/test.c
+++ stm32f091rc/programs/test/test.c
@@ -1,8 +1,7 @@
 #include <stm32f0xx.h>
 #include <delay.h>
-#include <usart.h>
+#include "usart_dma.h"
 
-#define I2C_DELIMITER '\n'
 #define LED_ON(led) GPIOB->BSRR |= (1 << led)
 #define LED_OFF(led) GPIOB->BSRR |= ((1 << led) << 16)
 #define LED_TOGGLE(led) GPIOB->ODR ^= (1 << led)
@@ -10,8 +9,7 @@
 #define REG_ADDR_1 0x0e
 #define REG_ADDR_2 0x00
 
-volatile uint8_t i2c1_rx_busy = 0; // status flag
-volatile uint8_t i2c1_tx_busy = 0; // status flag
+volatile uint8_t usart2_rx_busy;
 
 void init_leds(void);
 void init_i2c1(void);
@@ -24,33 +22,20 @@ void set_pwm(uint8_t i);
 void itoa(uint8_t* str, uint8_t len, uint32_t val);
 uint8_t zeroes(uint32_t reg);
 
-void DMA1_Ch2_3_DMA2_Ch1_2_IRQHandler(void) {
-	if((DMA1->ISR & DMA_ISR_TCIF2) == DMA_ISR_TCIF2) { // ch2 tc flag
-		i2c1_tx_busy = 0; // set to available
-		DMA1_Channel2->CCR &= ~(DMA_CCR_EN); // turn off periph
-		DMA1->IFCR |= DMA_IFCR_CTCIF2; // clear interrupt flag
-	}
-	if((DMA1->ISR & DMA_ISR_TCIF3) == DMA_ISR_TCIF3) { // ch3 tc flag
-		i2c1_rx_busy = 0; // set to available
-		DMA1_Channel3->CCR &= ~(DMA_CCR_EN); // turn off periph
-		DMA1->IFCR |= DMA_IFCR_CTCIF3; // clear interrupt flag
-	}
-}
-
 int main() {
 	init_leds();
 	init_delay();
-	init_i2c1();
+	init_usart2(9600);
 
-	uint8_t rtc_setup[] = {0b00010000, I2C_DELIMITER};
-	uint8_t read_data[5] = {0};
-
-	i2c1_send(SLAVE_ADDR, REG_ADDR_1, rtc_setup);
+	uint8_t read_data[3] = {0x67, 0x68};
 
 	while(1) {
-		_delay_ms(200);
-		i2c1_read(SLAVE_ADDR, REG_ADDR_2, read_data, 2);
-		GPIOB->ODR = (read_data[0] >> 4) * 10 + (read_data[0] & 0x0f);
+		usart2_read(read_data, 2);
+		LED_TOGGLE(5);
+		while(usart2_rx_busy);
+		usart2_send(read_data, 2);
+		_delay_ms(10);
+		LED_TOGGLE(6);
 	}
 
 	return 0;
@@ -61,96 +46,6 @@ void init_leds(void) {
 	RCC->AHBENR |= RCC_AHBENR_GPIOBEN; // turn clock on
 	GPIOB->MODER |= 0x5555UL; // set 8 pins to output
 }
-
-void init_i2c1(void) {
-	// init dma interrupt
-	NVIC_EnableIRQ(DMA1_Ch2_3_DMA2_Ch1_2_IRQn);
-	NVIC_SetPriority(DMA1_Ch2_3_DMA2_Ch1_2_IRQn, 2);
-	
-	// init pins
-	RCC->AHBENR |= RCC_AHBENR_GPIOBEN; // start clock
-	GPIOB->MODER |= (GPIO_MODER_MODER9_1 | GPIO_MODER_MODER8_1); // set af mode
-	GPIOB->AFR[1] |= 0x11U; // set af1
-	GPIOB->PUPDR |= (GPIO_PUPDR_PUPDR9_0 | GPIO_PUPDR_PUPDR8_0); // set pull ups
-	
-	// // init dma
-	RCC->AHBENR |= RCC_AHBENR_DMA1EN; // start clock
-	DMA1->CSELR |= (DMA1_CSELR_CH2_I2C1_TX | DMA1_CSELR_CH3_I2C1_RX); // set channel selection
-	// init channel2
-	DMA1_Channel2->CCR |= DMA_CCR_PL_1; // set high priority
-	DMA1_Channel2->CCR |= DMA_CCR_MINC; // set memory increment mode
-	DMA1_Channel2->CCR |= DMA_CCR_TCIE; // turn on tc interrupt
-	DMA1_Channel2->CCR |= DMA_CCR_DIR; // set read from memory
-	DMA1_Channel2->CCR &= ~(DMA_CCR_MSIZE); // set 8bit memory size
-	DMA1_Channel2->CCR &= ~(DMA_CCR_PSIZE); // set 8bit periph size
-	DMA1_Channel2->CPAR = (uint32_t)&(I2C1->TXDR); // set periph address
-	// init channel3
-	DMA1_Channel3->CCR |= DMA_CCR_PL_1; // set high priority
-	DMA1_Channel3->CCR |= DMA_CCR_MINC; // set memory increment mode
-	DMA1_Channel3->CCR |= DMA_CCR_TCIE; // turn on tc interrupt
-	DMA1_Channel3->CCR &= ~(DMA_CCR_DIR); // set read from periph
-	DMA1_Channel3->CCR &= ~(DMA_CCR_MSIZE); // set 8bit memory size
-	DMA1_Channel3->CCR &= ~(DMA_CCR_PSIZE); // set 8bit periph size
-	DMA1_Channel3->CPAR = (uint32_t)&(I2C1->RXDR); // set periph address
-	
-	// init i2c1
-	RCC->CFGR3 |= RCC_CFGR3_I2C1SW_SYSCLK; // select SysClock as clock source
-	RCC->APB1ENR |= RCC_APB1ENR_I2C1EN; // start clock
-	I2C1->TIMINGR = (uint32_t)0x10905F86; // set timing register (Sm 100kHz)
-	I2C1->CR1 |= I2C_CR1_PE; // enable periph
-	I2C1->CR1 |= (I2C_CR1_TXDMAEN | I2C_CR1_RXDMAEN); // enable dma on tx/rx
-}
-
-void i2c1_send(uint8_t slave_addr, uint8_t addr, uint8_t* data_ptr) {
-	uint8_t count = 0;
-
-	while(i2c1_tx_busy); // check if available
-	i2c1_tx_busy = 1; // set to busy
-	while(*(data_ptr + count) != I2C_DELIMITER) { // count number of bytes to send
-		count++;
-	}
-	for(int8_t i = count; i >= 0; i--) { // shift data array
-		*(data_ptr + i + 1) = *(data_ptr + i);
-	}
-	*(data_ptr) = addr; // set register address
-	count ? : count++; // fix shitty programming
-
-	I2C1->CR2 &= ~(I2C_CR2_RD_WRN); // set write direction
-	I2C1->CR2 &= ~(I2C_CR2_SADD); // clear address
-	I2C1->CR2 |= (slave_addr << 1); // set slave address
-	I2C1->CR2 |= I2C_CR2_AUTOEND; // enable autoend
-	I2C1->CR2 &= ~(I2C_CR2_NBYTES); // clear number of bytes
-	I2C1->CR2 |= (count << 16); // set number of bytes to send
-
-	DMA1_Channel2->CMAR = (uint32_t)data_ptr; // set memory address
-	DMA1_Channel2->CNDTR = count; // set number of bytes to send
-	DMA1_Channel2->CCR |= DMA_CCR_EN; // turn on perpiph
-
-	I2C1->CR2 |= I2C_CR2_START; // start transmition
-}
-
-void i2c1_read(uint8_t slave_addr, uint8_t addr, uint8_t* data_ptr, uint8_t count) {
-	uint8_t dummy[] = {I2C_DELIMITER}; // set dummy array
-	
-	while(i2c1_rx_busy); // check if available
-	i2c1_rx_busy = 1; // set to busy
-	i2c1_send(slave_addr, addr, dummy); // set register to read from
-	while(i2c1_tx_busy); // wait for delivery
-
-	I2C1->CR2 |= I2C_CR2_RD_WRN; // set read direction
-	I2C1->CR2 |= I2C_CR2_AUTOEND; // enable autoend
-	I2C1->CR2 &= ~(I2C_CR2_SADD); // clear address
-	I2C1->CR2 |= (slave_addr << 1); // set slave address
-	I2C1->CR2 &= ~(I2C_CR2_NBYTES); // clear number of bytes
-	I2C1->CR2 |= (count << 16); // set number of bytes to send
-	
-	DMA1_Channel3->CMAR = (uint32_t)data_ptr; // set memory address
-	DMA1_Channel3->CNDTR = count; // set number of bytes to read
-	DMA1_Channel3->CCR |= DMA_CCR_EN; // turn on periph
-
-	I2C1->CR2 |= I2C_CR2_START; // start transmition
-}
-
 
 void init_adc(void) {
 	// init pin
---
+++ stm32f091rc/programs/usart_dma/usart_dma.c
@@ -0,0 +1,71 @@
+#include <stm32f0xx.h>
+#include "usart_dma.h"
+
+volatile uint8_t usart2_rx_busy = 0; // status flag
+volatile uint8_t usart2_tx_busy = 0; // status flag
+
+void DMA1_Ch2_3_DMA2_Ch1_2_IRQHandler(void) {
+	if((DMA2->ISR & DMA_ISR_TCIF1) == DMA_ISR_TCIF1) { // ch1 tc flag
+		usart2_tx_busy = 0; // set to available
+		DMA2_Channel1->CCR &= ~(DMA_CCR_EN); // turn off periph
+		DMA2->IFCR |= DMA_IFCR_CTCIF1; // clear interrupt flag
+	}
+	if((DMA2->ISR & DMA_ISR_TCIF2) == DMA_ISR_TCIF2) { // ch2 tc flag
+		usart2_rx_busy = 0; // set to available
+		DMA2_Channel2->CCR &= ~(DMA_CCR_EN); // turn off periph
+		DMA2->IFCR |= DMA_IFCR_CTCIF2; // clear interrupt flag
+	}
+}
+
+void init_usart2(uint16_t baud) {
+	// init dma interrupt
+	NVIC_EnableIRQ(DMA1_Ch2_3_DMA2_Ch1_2_IRQn);
+	NVIC_SetPriority(DMA1_Ch2_3_DMA2_Ch1_2_IRQn, 2);
+	
+	// init pins
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN; // start clock
+	GPIOA->MODER |= (GPIO_MODER_MODER3_1 | GPIO_MODER_MODER2_1); // set af mode
+	GPIOA->AFR[0] |= 0x1100U; // set af1
+	
+	// init dma
+	RCC->AHBENR |= RCC_AHBENR_DMA2EN; // start clock
+	DMA2->CSELR |= (DMA2_CSELR_CH1_USART2_TX | DMA2_CSELR_CH2_USART2_RX); // set channel selection
+	// init channel1
+	DMA2_Channel1->CCR |= DMA_CCR_PL_1; // set high priority
+	DMA2_Channel1->CCR |= DMA_CCR_MINC; // set memory increment mode
+	DMA2_Channel1->CCR |= DMA_CCR_DIR; // set read from memory
+	DMA2_Channel1->CCR |= DMA_CCR_TCIE; // turn on tc interrupt
+	DMA2_Channel1->CCR &= ~(DMA_CCR_MSIZE); // set 8bit memory size
+	DMA2_Channel1->CCR &= ~(DMA_CCR_PSIZE); // set 8bit periph size
+	DMA2_Channel1->CPAR = (uint32_t)&(USART2->TDR); // set periph address
+	// init channel2
+	DMA2_Channel2->CCR |= DMA_CCR_PL_1; // set high priority
+	DMA2_Channel2->CCR |= DMA_CCR_MINC; // set memory increment mode
+	DMA2_Channel2->CCR |= DMA_CCR_TCIE; // turn on tc interrupt
+	DMA2_Channel2->CCR &= ~(DMA_CCR_DIR); // set read from periph
+	DMA2_Channel2->CCR &= ~(DMA_CCR_MSIZE); // set 8bit memory size
+	DMA2_Channel2->CCR &= ~(DMA_CCR_PSIZE); // set 8bit periph size
+	DMA2_Channel2->CPAR = (uint32_t)&(USART2->RDR); // set periph address
+	
+	// init usart
+	RCC->APB1ENR |= RCC_APB1ENR_USART2EN; // start clock
+	USART2->BRR = SystemCoreClock / baud; // set baud rate
+	USART2->CR3 |= (USART_CR3_DMAT | USART_CR3_DMAR); // enable dma on tx/rx
+	USART2->CR1 |= (USART_CR1_TE | USART_CR1_RE | USART_CR1_UE); // enable periph
+}
+
+void usart2_send(uint8_t* data_ptr, uint16_t count) {
+	while(usart2_tx_busy); // check if available
+	usart2_tx_busy = 1; // set to busy
+	DMA2_Channel1->CNDTR = count; // set number of bytes to send
+	DMA2_Channel1->CMAR = (uint32_t)data_ptr; // set memory address
+	DMA2_Channel1->CCR |= DMA_CCR_EN; // turn on periph
+}
+
+void usart2_read(uint8_t* data_ptr, uint16_t count) {
+	while(usart2_rx_busy); // check if available
+	usart2_rx_busy = 1; // set to busy
+	DMA2_Channel2->CNDTR = count; // set number of bytes to read
+	DMA2_Channel2->CMAR = (uint32_t)data_ptr; // set memory address
+	DMA2_Channel2->CCR |= DMA_CCR_EN; // turn on periph
+}
